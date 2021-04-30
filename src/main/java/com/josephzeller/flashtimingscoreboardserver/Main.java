f3be607b190d1e69e2fe0fa0ffbf170dac0d2274
jamesjulich <51384945+jamesjulich@users.noreply.github.com>
2021-04-29 22:06:50 -0500

    Basic main method.

--- src/main/java/com/josephzeller/flashtimingscoreboardserver/Main.java
+++ src/main/java/com/josephzeller/flashtimingscoreboardserver/Main.java
@@ -2,5 +2,10 @@ package com.josephzeller.flashtimingscoreboardserver;
 
 public class Main 
 {
-    
+    public static void main(String[] args)
+    {
+        ScoreboardGUI gui = new ScoreboardGUI();
+        gui.setVisible(true);
+        gui.setResizable(false);
+    }
 }
